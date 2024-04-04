81d59fbc9c2dab4692af6fd8f71fa499874e8360
Evandro2270 <150724259+Evandro2270@users.noreply.github.com>
2024-04-04 16:05:39 -0300

    Adicionar arquivos de projeto.

---
+++ IfElse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+                
+                Console.WriteLine("Digite a hora atual: ");
+                int hora = int.Parse(Console.ReadLine());
+
+
+                if (hora < 12)
+                {
+                    Console.WriteLine("Bom dia!");
+                }
+
+                else if (hora < 18)
+                {
+                    Console.WriteLine("Boa tarde!");
+                }
+
+                else {
+                    Console.WriteLine("Boa Noite");
+                }
+
+              }
+        }
+    }
+}
